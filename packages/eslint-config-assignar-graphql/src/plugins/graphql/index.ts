ff4c0ac89a7ba9bd8bd70cc933a7e97758e66533
Brad Zacher <brad.zacher@gmail.com>
2018-07-04 14:25:47 +1000

    [FIX] remove defunct try/catch code

--- packages/eslint-config-assignar-graphql/src/plugins/graphql/index.ts
+++ packages/eslint-config-assignar-graphql/src/plugins/graphql/index.ts
@@ -1,5 +1,3 @@
-/* eslint-disable import/no-mutable-exports */
-
 import personalJson = require('./schemas/personal.json')
 import mutationJson = require('./schemas/mutation.json')
 import standardJson = require('./schemas/standard.json')
@@ -8,66 +6,57 @@ import reportingJson = require('./schemas/reporting.json')
 // TODO - figure out how to make the schema imports independent of this repo...
 
 // don't configure the graphql ruleset if graphql is not installed
-let config : Record<'rules' | 'name', any> = {} as any
-try {
-    // this will throw if graphql is not installed
-    require.resolve('graphql')
-    const ruleConfig = [
-        // add config for each endpoint we touch
-        {
-            env: 'apollo',
-            tagName: 'gqlPersonal',
-            schemaJson: personalJson,
-        },
-        {
-            env: 'apollo',
-            tagName: 'gqlMutation',
-            schemaJson: mutationJson,
-        },
-        {
-            env: 'apollo',
-            tagName: 'gqlStandard',
-            schemaJson: standardJson,
-        },
-        {
-            env: 'apollo',
-            tagName: 'gqlReporting',
-            schemaJson: reportingJson,
-        },
-    ]
+const ruleConfig = [
+    // add config for each endpoint we touch
+    {
+        env: 'apollo',
+        tagName: 'gqlPersonal',
+        schemaJson: personalJson,
+    },
+    {
+        env: 'apollo',
+        tagName: 'gqlMutation',
+        schemaJson: mutationJson,
+    },
+    {
+        env: 'apollo',
+        tagName: 'gqlStandard',
+        schemaJson: standardJson,
+    },
+    {
+        env: 'apollo',
+        tagName: 'gqlReporting',
+        schemaJson: reportingJson,
+    },
+]
 
-    config = {
-        name: 'graphql',
-        rules: {
-            'graphql/template-strings': [
-                'error',
-                ...ruleConfig,
-            ],
+export default {
+    name: 'graphql',
+    rules: {
+        'graphql/template-strings': [
+            'error',
+            ...ruleConfig,
+        ],
 
-            // The Named Operation rule validates that all operations are named. Naming operations is valuable for including in server-side logs and debugging.
-            'graphql/named-operations': [
-                'error',
-                ...ruleConfig,
-            ],
+        // The Named Operation rule validates that all operations are named. Naming operations is valuable for including in server-side logs and debugging.
+        'graphql/named-operations': [
+            'error',
+            ...ruleConfig,
+        ],
 
-            // The Required Fields rule validates that any specified required field is part of the query, but only if that field is available in schema. This is useful to ensure that query results are cached properly in the client.
-            'graphql/required-fields': 'off',
+        // The Required Fields rule validates that any specified required field is part of the query, but only if that field is available in schema. This is useful to ensure that query results are cached properly in the client.
+        'graphql/required-fields': 'off',
 
-            // This rule enforces that first letter of types is capitalized
-            'graphql/capitalized-type-name': [
-                'error',
-                ...ruleConfig,
-            ],
+        // This rule enforces that first letter of types is capitalized
+        'graphql/capitalized-type-name': [
+            'error',
+            ...ruleConfig,
+        ],
 
-            // The No Deprecated Fields rule validates that no deprecated fields are part of the query. This is useful to discover fields that have been marked as deprecated and shouldn't be used.
-            'graphql/no-deprecated-fields': [
-                'error',
-                ...ruleConfig,
-            ],
-        },
-    }
-} catch (_) {
-    // ignored
+        // The No Deprecated Fields rule validates that no deprecated fields are part of the query. This is useful to discover fields that have been marked as deprecated and shouldn't be used.
+        'graphql/no-deprecated-fields': [
+            'error',
+            ...ruleConfig,
+        ],
+    },
 }
-
-export default config
